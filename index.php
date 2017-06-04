01d3533bd172325a61303221f86a74ced701e7dc
littlebetter <1209931797@qq.com>
2017-06-04 10:26:05 +0800

    index.php 基础之重

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+var_dump($_POST);
+var_dump($_GET);
+var_dump($_REQUEST);
+var_dump($_SERVER);
+var_dump($_COOKIE);
+var_dump($_SESSION);
+var_dump($_ENV);
+var_dump($_FILES);
+
+echo __LINE__;
+echo __FILE__;
+echo __METHOD__;
+echo __DIR__;
+echo __CLASS__;
+echo __NAMESPACE__;
+echo __FUNCTION__;
